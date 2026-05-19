9ba3202ef9d99c4c1a6f89ba180cfa18dee3f19b
Mir <mtr5578@psu.edu>
2026-05-19 16:41:26 -0400

    fix fake lidar

--- mrlib/lidar_controller_fake.py
+++ mrlib/lidar_controller_fake.py
@@ -7,6 +7,7 @@ class LidarController:
     """
     Manages LiDAR hardware and provides thread-safe access to the latest scan data.
     """
+    
     def __init__(self):
         self.lidar = PyRPlidar()
         self.running = False
@@ -19,8 +20,6 @@ class LidarController:
         """Connect to the LiDAR sensor and start the background data thread."""
         self.lidar.connect(port=port, baudrate=baudrate, timeout=timeout)
         self.running = True
-        self.worker_thread = threading.Thread(target=self._lidar_worker, daemon=True)
-        self.worker_thread.start()
         self.motor_started = False
         print(f"[INFO] LiDAR connected on {port}")
 
@@ -31,41 +30,36 @@ class LidarController:
         n = 0
         prior_running_angle = 0
         running_angle = 0
-
+        scan_generator = self.lidar.start_scan()
         while not self.motor_started:
             time.sleep(0.2)
 
-        try:
-            scan_generator = self.lidar.start_scan()
-            for scan in scan_generator:
-                if not self.running:
-                    break
-                # Get from the scan point
-                dist = scan.distance
-                angle = scan.angle
+        for scan in scan_generator:
+            if not self.running:
+                break
+            # Get from the scan point
+            dist = scan.distance
+            angle = scan.angle
 
-                if n < Max_points_per_set:
-                    a_scan_set[n,0] = dist
-                    a_scan_set[n,1] = angle
-                    # Update count
-                    n += 1
-                # Calculate new running angle
-                running_angle = 0.9 * prior_running_angle + 0.1 * angle
-                # Check if complete set
-                if prior_running_angle > 180 and running_angle < 180:
-                    # # Update the latest scan in a thread-safe manner
-                    with self._data_lock:
-                        self.latest_scan_set = a_scan_set[:n,:]
-                    # Make new object
-                    a_scan_set = np.empty((Max_points_per_set, 2)) 
-                    # Reset count
-                    n = 0
+            if n < Max_points_per_set:
+                a_scan_set[n,0] = dist
+                a_scan_set[n,1] = angle
+                # Update count
+                n += 1
+            # Calculate new running angle
+            running_angle = 0.9 * prior_running_angle + 0.1 * angle
+            # Check if complete set
+            if prior_running_angle > 180 and running_angle < 180:
+                # # Update the latest scan in a thread-safe manner
+                with self._data_lock:
+                    self.latest_scan_set = a_scan_set[:n,:]
+                # Make new object
+                a_scan_set = np.empty((Max_points_per_set, 2)) 
+                # Reset count
+                n = 0
 
-                #Update running angle
-                prior_running_angle = running_angle
-
-        except Exception as e:
-            print(f"[ERROR] LiDAR Worker Error: {e}")
+            #Update running angle
+            prior_running_angle = running_angle
 
     def set_motor_pwm(self, pwm: int) -> None:
         """Set the motor speed (typically 600 for standard rotation)."""
@@ -73,6 +67,10 @@ class LidarController:
         time.sleep(0.2)
         self.motor_started = True
         print(f"[INFO] LiDAR motor PWM set to {pwm}")
+        time.sleep(2)  # Give the motor a moment to start before we begin scanning
+        self.worker_thread = threading.Thread(target=self._lidar_worker, daemon=True)
+        self.worker_thread.start()
+
 
     def get_scan(self) -> np.ndarray:
         """
